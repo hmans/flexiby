ea9bd1c2a08143518f29d23dc1f13131890b2323
Hendrik Mans <hendrik@mans.de>
2017-01-12 12:50:09 +0100

    Render in series

--- lib/flexiby/node.ex
+++ lib/flexiby/node.ex
@@ -1,8 +1,39 @@
 defmodule Flexiby.Node do
-  defstruct code: nil, name: nil, ext: nil, filters: []
+  defstruct fs_path: nil,
+    source: nil,
+    body: nil,
+    name: nil,
+    ext: nil,
+    filters: []
 
-  def render_to_string(node, bindings \\ [], options \\ []) do
-    {result, _} = Code.eval_quoted(node.code, bindings, options)
-    result
+  def from(path) do
+    source   = File.read!(path)
+    filename = Path.basename(path)
+    [name, ext | filters] = String.split(filename, ".", trim: true)
+
+    %__MODULE__{
+      fs_path: path,
+      source:  source,
+      name:    name,
+      ext:     ext,
+      filters: filters
+    }
+  end
+
+  def render(node) do
+    Enum.reduce node.filters,
+      %{node | body: node.source },
+      fn(f, node) ->
+        apply_filter(node, f)
+      end
+  end
+
+  def apply_filter(node, "eex") do
+    %{ node | body: EEx.eval_string(node.body) }
+  end
+
+  def apply_filter(node, filter) do
+    IO.puts "Unknown filter '#{filter}' for #{Path.basename(node.fs_path)}"
+    node
   end
 end
--- lib/flexiby/test_plug.ex
+++ lib/flexiby/test_plug.ex
@@ -2,17 +2,16 @@ defmodule Flexiby.TestRouter do
   use Plug.Router
 
   alias Flexiby.Node
-  alias Flexiby.Builder
 
   plug :match
   plug :dispatch
 
-  def output do
-    Builder.from("./site/index.html.eex.foo")
-    |> Node.render_to_string
+  def test_node do
+    Node.from("./site/index.html.eex.foo")
+    |> Node.render
   end
 
   match _ do
-    conn |> send_resp(200, output())
+    conn |> send_resp(200, test_node().body)
   end
 end
