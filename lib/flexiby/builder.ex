a88e425cd40dd4368514741f689979402173260f
Hendrik Mans <hendrik@mans.de>
2017-01-12 12:32:32 +0100

    Walk through multiple extensions

--- lib/flexiby/builder.ex
+++ lib/flexiby/builder.ex
@@ -5,12 +5,17 @@ defmodule Flexiby.Builder do
     source   = File.read!(path)
     filename = Path.basename(path)
     [name, ext | filters] = String.split(filename, ".", trim: true)
-    code = compile(:eex, source)
+
+    code = Enum.map(filters, fn(f) -> compile(f, source) end)
 
     %Node{code: code, name: name, ext: ext, filters: filters}
   end
 
-  defp compile(:eex, source, options \\ []) do
-    EEx.compile_string(source, options)
+  defp compile("eex", source) do
+    EEx.compile_string(source)
+  end
+
+  defp compile(_, _source) do
+    []
   end
 end
