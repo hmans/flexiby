83b7c68950c74b4952055d18fb5edaff647df7ec
Hendrik Mans <hendrik@mans.de>
2017-01-12 10:45:02 +0100

    New mix project

---
+++ lib/flexiby.ex
@@ -0,0 +1,18 @@
+defmodule Flexiby do
+  @moduledoc """
+  Documentation for Flexiby.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Flexiby.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/flexiby_test.exs
@@ -0,0 +1,8 @@
+defmodule FlexibyTest do
+  use ExUnit.Case
+  doctest Flexiby
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
